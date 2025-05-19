4adc4a91312669d7de788bec3d648b2c663e6fac
Suphachok Learmsingam <paosos123@gmail.com>
2025-05-19 16:39:49 +0700

    Revert "Add Click to get point"
    This reverts commit df72929bbb1d632da51394f06a2003991d5c1835.

--- Assets/Scripts/ClickToAddPoints.cs
+++ Assets/Scripts/ClickToAddPoints.cs
@@ -1,13 +1,12 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class ClickToAddPoints : MonoBehaviour
 {
-    public int points = 0; // total score
-    public int pointsPerClick = 1; // points added per click
-    public Text pointsText; // UI score text
+    public int points = 0; //Total Scores
+    public int pointsPerClick = 1; // Score per Click
+    public TMP_Text pointsText; // UI Score Text
 
     void Start()
     {
